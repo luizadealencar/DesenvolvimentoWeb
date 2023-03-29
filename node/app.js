08692d801b40bad75ce22524fb54c5823233f562
Luiza Alencar <66708154+luizadealencar@users.noreply.github.com>
2023-03-29 08:22:29 -0300

    29/03

--- node/app.js
+++ node/app.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
+import express from 'express'
+import { engine } from 'express-handlebars'
 
+//const express = require('express')
+const app = express()
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+app.get("/", (req, res) => {res.render(home) })
+app.get("/sobre", (req, res) => {res.render})
 app.use(express.static('./public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
